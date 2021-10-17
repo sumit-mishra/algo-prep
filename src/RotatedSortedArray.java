97f02607d4989c019066444a575366b675a7ea42
sumit.mishra <aumitmishra011@gmail.com>
2021-10-17 23:53:59 +0530

    code with test cases, formatting updated

--- src/RotatedSortedArray.java
+++ src/RotatedSortedArray.java
@@ -2,11 +2,12 @@ public class RotatedSortedArray {
 
     public static void main(String[] args) {
         int[] case1 = new int[]{60,70,80,90,10,20,30,40,50};
-        int[] case2 = new int[] {20,30,40,50,60,70,80,90,10};
-        int[] case3 = new int[] {90,10,20,30,40,50,60,70,80};
-
         System.out.println(getMinElement(case1));
+
+        int[] case2 = new int[] {20,30,40,50,60,70,80,90,10};
         System.out.println(getMinElement(case2));
+
+        int[] case3 = new int[] {90,10,20,30,40,50,60,70,80};
         System.out.println(getMinElement(case3));
     }
 
